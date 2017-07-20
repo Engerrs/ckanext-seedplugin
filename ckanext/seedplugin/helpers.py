bfad435cce2e308b3bdd674bb5973a71ba4c12ad
Gregory von Nessi <gvn@Derp-E.local>
2017-07-20 14:06:18 +1000

    Bugfix for retrieving sitecore URL

--- ckanext/seedplugin/helpers.py
+++ ckanext/seedplugin/helpers.py
@@ -14,7 +14,7 @@ def get_seed_helpers():
         'seed_facet_list': seed_facet_list,
         'seed_facet_remove': seed_facet_remove,
         'seed_all_facets_remove': seed_all_facets_remove,
-        'get_sitecore_base_url': get_geocortex_base_url,
+        'get_sitecore_base_url': get_sitecore_base_url,
         'get_geocortex_base_url': get_geocortex_base_url,
         'visualise_on_map_has_web_map_layer': has_web_map_layer,
         'visualise_on_map_get_web_map_layer_url': get_web_map_layer_url
