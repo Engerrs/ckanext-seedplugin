884ddc4c078df1751674d57eacb666b1575b98ba
Gregory von Nessi <gvn@Derp-E.local>
2017-07-27 11:36:24 +1000

    Refactor so that Geocortex baseurl config contains viewer and local parameters

--- ckanext/seedplugin/plugin.py
+++ ckanext/seedplugin/plugin.py
@@ -256,8 +256,7 @@ class SeedpluginPlugin(plugins.SingletonPlugin):
         pkg = model.Package.get(resource_dict['package_id'])
         seedwebmap = 'SEED Web Map'
         if resource_dict['format'].lower() == seedwebmap.lower() and 'map_type' in pkg.extras and 'layer_list_name' in pkg.extras and 'layer_catalog_name' in pkg.extras and 'map_service_id' in pkg.extras:
-            # resource_dict['url'] = 'https://geo.seed.nsw.gov.au/EDP_Public_Viewer/Index.html?viewer=EDP_Public_Viewer&run=ViewMap&url='+pkg.extras['map_type']+":map_service_id="+pkg.extras['map_service_id'].replace("&","+")+";layer_id="+pkg.extras['layer_list_name'].replace("&","+")
-            resource_dict['url'] = seed_helpers.get_geocortex_base_url() + '?viewer=EDP_DEV_Viewer&locale=en-AU&runWorkflow=AppendLayerCatalog&CatalogLayer=' + pkg.extras['layer_catalog_name'] +'.' + pkg.extras['map_service_id'] + '.' + pkg.extras['layer_list_name'].replace(' ', '%20') +'&MapServiceID='+ pkg.extras['map_service_id'] + '&LayerListName=' + pkg.extras['layer_list_name'].replace(' ', '%20')
+            resource_dict['url'] = seed_helpers.get_geocortex_base_url() + '&runWorkflow=AppendLayerCatalog&CatalogLayer=' + pkg.extras['layer_catalog_name'] +'.' + pkg.extras['map_service_id'] + '.' + pkg.extras['layer_list_name'].replace(' ', '%20') +'&MapServiceID='+ pkg.extras['map_service_id'] + '&LayerListName=' + pkg.extras['layer_list_name'].replace(' ', '%20')
         return resource_dict
 
     def before_search(self, search_params):
